ed760353673ecfc4283add881fe007c8546796ef
Harris Kirk <hkirk@copyright.com>
2014-09-19 15:03:56 -0400

    WORKS OK - spring-aop 'dependencies' vs 'dependencyInsight' But: pAD shows first level deps only

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@ clear
 set -x
 
 # shows trans deps
-gradlew dependencies --configuration runtime
+gradlew dependencies --configuration compile
+
+# shows reverse dependency tree
+gradlew -q dependencyInsight --dependency spring-aop --configuration compile
 
 # shows only first level deps
-#gradlew  -PconsumedGroup=.+ printAD
+gradlew  -PconsumedGroup=org.springframe.+ printAD
 
-gradlew -q dependencyInsight --dependency spring-beans --configuration runtime
+
 
