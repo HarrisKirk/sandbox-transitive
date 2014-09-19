0272bfc931eb3527844678b101f338215fd5e8ad
Harris Kirk <hkirk@copyright.com>
2014-09-19 14:46:54 -0400

    Ensure both runtime configs

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
+clear
+set -x
 
 # shows trans deps
 gradlew dependencies --configuration runtime
 
 # shows only first level deps
-gradlew  -PconsumedGroup=.+ printAD
+#gradlew  -PconsumedGroup=.+ printAD
+
+gradlew -q dependencyInsight --dependency spring-beans --configuration runtime
 
